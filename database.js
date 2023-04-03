e6cc72b4e8ce5890c85c2a27519096435184245a
Hung Hsin Li <65617015+licjn@users.noreply.github.com>
2023-04-03 16:53:31 -0400

    Update database.js

--- database.js
+++ database.js
@@ -2,17 +2,17 @@ const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database(":memory:");
 
 db.serialize(() => {
-  db.run('CREATE TABLE professors (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL)');
+  db.run('CREATE TABLE professors (id INTEGER PRIMARY KEY AUTOINCREMENT, schoolID INTEGER UNIQUE NOT NULL name TEXT NOT NULL)');
   db.run('CREATE TABLE reviews (id INTEGER PRIMARY KEY AUTOINCREMENT, professorID INTEGER REFERENCES professors(id), university TEXT NOT NULL, rating INTEGER, review TEXT)');
   // Trigger checks rating is between 0 and 10 before inserting new row
   db.run('CREATE TRIGGER check_rating BEFORE INSERT ON reviews WHEN NEW.rating < 0 OR NEW.rating > 10 BEGIN SELECT RAISE(ABORT, "Rating must be between 0 and 10"); END;');
 });
 
 // Create Data
-db.addProfessor = (profName) => {
+db.addProfessor = (schoolID, profName) => {
   let success = false;
     try {
-      success = db.run("INSERT INTO PROFESSORS (name) VALUES (?)", [profName]);   
+      success = db.run("INSERT INTO PROFESSORS (name) VALUES (?, ?)", [schoolID, profName]);   
     } catch (dbError) {
       console.error(dBerror);
     }
@@ -77,7 +77,7 @@ db.deleteReview = (id) => {
 // database function to return all reviews for a professor
 // wrap this in a function that takes in the id of the professor and returns the reviews
 db.getReviews = (id) => {
-  db.all("SELECT professors.name, reviews.university, reviews.rating, reviews.review FROM reviews JOIN professors ON reviews.professorID = professors.id WHERE reviews.professorID = ?", [id], (err, rows) => {
+  db.all("SELECT professors.name, reviews.university, reviews.rating, reviews.review FROM reviews JOIN professors ON reviews.professorID = professors.id WHERE professors.schoolID = ?", [schoolID], (err, rows) => {
     if (err) {
       console.error("Error getting reviews from database:", err);
       return rows;
@@ -87,10 +87,37 @@ db.getReviews = (id) => {
   });
 };
 
-db.getProfessor = (id) => {
+db.getProfessorID = (id) => {
   db.all("SELECT * FROM professors ON WHERE id = ?", [id], (err, rows) => {
     if (err) {
-      console.error("Error getting professor from database:", err);
+      console.error("Error getting professor from the database:", err);
+      return rows;
+    } else {
+      return rows;
+    }
+  });
+};
+
+
+// Search by professor name
+db.getProfessorName = (name) => {
+  db.all("SELECT * FROM professors ON WHERE id = ?", [name], (err, rows) => {
+    if (err) {
+      console.error("Error getting professor from the database:", err);
+      return rows;
+    } else {
+      return rows;
+    }
+  });
+};
+
+
+
+// Search by professor name
+db.getProfessorSchoolID = (schoolID) => {
+  db.all("SELECT * FROM professors ON WHERE id = ?", [schoolID], (err, rows) => {
+    if (err) {
+      console.error("Error getting professor from the database:", err);
       return rows;
     } else {
       return rows;
